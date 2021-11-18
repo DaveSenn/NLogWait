71b186b291e82b662d8bfbc8b6f34b1595cb94d2
Dave Senn <dave.jon.senn@gmail.com>
2021-11-18 11:13:33 +0100

    .NET 5

--- NLogWait/Program.cs
+++ NLogWait/Program.cs
@@ -1,29 +1,37 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Extensions.Hosting;
 using NLogWait;
 
-Console.Title = $"LogDemo - {Environment.ProcessId} (PID)";
-
-var builder = Host.CreateDefaultBuilder( args );
-
-var host = builder
-           .ConfigureServices( services =>
-           {
-               services.AddLogging();
-               services.AddHostedService<Worker>();
-           } )
-           .ConfigureLogging( logging => logging.ClearProviders() )
-           .UseNLog()
-           .Build();
-
-try
+public static class Program
 {
-    await host.RunAsync();
-}
-finally
-{
-    LogManager.Shutdown();
+    public static async Task Main()
+    {
+        Console.Title = $"LogDemo - {Environment.ProcessId} (PID)";
+
+        var builder = Host.CreateDefaultBuilder();
+
+        var host = builder
+                   .ConfigureServices( services =>
+                   {
+                       services.AddLogging();
+                       services.AddHostedService<Worker>();
+                   } )
+                   .ConfigureLogging( logging => logging.ClearProviders() )
+                   .UseNLog()
+                   .Build();
+
+        try
+        {
+            await host.RunAsync();
+        }
+        finally
+        {
+            LogManager.Shutdown();
+        }
+    }
 }
--- NLogWait/Worker.cs
+++ NLogWait/Worker.cs
@@ -1,25 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace NLogWait;
-
-public sealed class Worker : BackgroundService
+namespace NLogWait
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker( ILogger<Worker> logger ) =>
-        _logger = logger;
-
-    protected override Task ExecuteAsync( CancellationToken stoppingToken )
+    public sealed class Worker : BackgroundService
     {
-        Parallel.For( 0,
-                      100,
-                      _ =>
-                      {
-                          while ( !stoppingToken.IsCancellationRequested )
-                              _logger.LogInformation( "Test" );
-                      } );
+        private readonly ILogger<Worker> _logger;
 
-        return Task.CompletedTask;
+        private readonly List<Task> _tasks = new(128);
+
+        private readonly List<Thread> _threads = new(128);
+
+        public Worker( ILogger<Worker> logger ) =>
+            _logger = logger;
+
+        protected override async Task ExecuteAsync( CancellationToken stoppingToken )
+        {
+            //await V2( stoppingToken );
+            await V3( stoppingToken );
+
+            Parallel.For( 0,
+                          100,
+                          //new() { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 },
+                          _ =>
+                          {
+                              while ( !stoppingToken.IsCancellationRequested )
+                                  _logger.LogInformation( "Test" );
+                          } );
+        }
+
+        private async Task V2( CancellationToken stoppingToken )
+        {
+            for ( var i = 0; i < 100; i++ )
+                _tasks.Add( Task.Run( () =>
+                                      {
+                                          while ( !stoppingToken.IsCancellationRequested )
+                                              _logger.LogInformation( "Test" );
+                                      },
+                                      CancellationToken.None ) );
+
+            await Task.WhenAll( _tasks );
+        }
+
+        private async Task V3( CancellationToken stoppingToken )
+        {
+            for ( var i = 0; i < 100; i++ )
+            {
+                var t = new Thread( () =>
+                {
+                    while ( !stoppingToken.IsCancellationRequested )
+                        _logger.LogInformation( "Test" );
+                } );
+                t.Start();
+                _threads.Add( t );
+            }
+
+            await Task.WhenAll( _tasks );
+        }
     }
 }
